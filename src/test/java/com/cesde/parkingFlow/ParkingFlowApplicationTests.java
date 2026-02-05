4050e8a9661874710a48183987afea5cf851a48d
Samuel <smazo870@gmail.com>
2026-02-05 12:09:53 -0500

    Mi primer parqueadero

---
+++ src/test/java/com/cesde/parkingFlow/ParkingFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesde.parkingFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
