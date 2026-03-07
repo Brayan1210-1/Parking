5ba8ad4e5aff76d81e815a97da17aa3372c4a20a
Samuel-MAZO1 <samuelmazo3954@gmail.com>
2026-03-07 01:56:50 -0500

    Delete src/main/java/com/cesde/parkingflow directory

--- src/main/java/com/cesde/parkingflow/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cesde.parkingflow.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
